80b9df46e3c029d9e1e1d77acc2473b82a1fdf46
AminTabite <Aminepicode13@gmail.com>
2025-09-12 15:37:31 +0200

    Update Main.java
    sbagliato main , riprovo

--- src/Entities/Main.java
+++ src/Entities/Main.java
@@ -3,6 +3,15 @@ package Entities;
 import java.util.Scanner;
 
 public class Main {
+    {
+        System.out.println("Ciao, scegli un numero da 1 a 5 per riprodurre un elemento multimediale");
+
+        int n = scanner.nextInt();
+
+    }
+
+    ;
+
     public static void main(String[] args) {
 
         RegistrazioneAudio Song1 = new RegistrazioneAudio(4, "We will rock you");
@@ -13,34 +22,14 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        ElementoMultimediale[] Lettoremultimediale = new ElementoMultimediale [5];
+        ElementoMultimediale[] Lettoremultimediale = new ElementoMultimediale[5];
         Lettoremultimediale[0] = Song1;
         Lettoremultimediale[1] = Song1;
         Lettoremultimediale[2] = gattojpg;
         Lettoremultimediale[3] = BionicleLore;
         Lettoremultimediale[4] = movie;
 
-    };
-
-
-
-
-        while(true) {
-            System.out.println("Ciao, scegli un numero da 1 a 5 per riprodurre un elemento multimediale");
-
-            int n = scanner.nextInt();
-
-
-
-
-            if (n ==0) {
-                
-            break;}
-            }
-
-        }
-
     }
 
-
 }
+
