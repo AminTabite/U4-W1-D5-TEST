a597961f346e3e94efbc60c70432bfb4f1137b54
AminTabite <Aminepicode13@gmail.com>
2025-09-12 16:07:02 +0200

    reinizio main
    .

--- src/Entities/Main.java
+++ src/Entities/Main.java
@@ -3,31 +3,28 @@ package Entities;
 import java.util.Scanner;
 
 public class Main {
-    {
-        System.out.println("Ciao, scegli un numero da 1 a 5 per riprodurre un elemento multimediale");
-
-        int n = scanner.nextInt();
-
-    }
-
-    ;
 
     public static void main(String[] args) {
-
-        RegistrazioneAudio Song1 = new RegistrazioneAudio(4, "We will rock you");
-        RegistrazioneAudio Song2 = new RegistrazioneAudio(4, "Preparation ");
-        Img gattojpg = new Img("gattojpg");
-        Video BionicleLore = new Video(10, "bioniclePirakastory");
-        Video movie = new Video(90, "Halloween");
-
         Scanner scanner = new Scanner(System.in);
 
         ElementoMultimediale[] Lettoremultimediale = new ElementoMultimediale[5];
-        Lettoremultimediale[0] = Song1;
-        Lettoremultimediale[1] = Song1;
-        Lettoremultimediale[2] = gattojpg;
-        Lettoremultimediale[3] = BionicleLore;
-        Lettoremultimediale[4] = movie;
+
+
+        System.out.println("Ciao, cerca dei file inserendo durata e titolo");
+        for (int i = 0; i <= Lettoremultimediale.length; i++) {
+                
+        }
+
+
+
+
+
+        /*RegistrazioneAudio Song1 = new RegistrazioneAudio(4, "We will rock you");
+        RegistrazioneAudio Song2 = new RegistrazioneAudio(4, "Preparation ");
+        Img gattojpg = new Img("gattojpg");
+        Video BionicleLore = new Video(10, "bioniclePirakastory");
+        Video movie = new Video(90, "Halloween");*/
+
 
     }
 
