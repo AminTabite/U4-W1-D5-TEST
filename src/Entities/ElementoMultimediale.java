b91829c3a8773008dc42a553c5de0b007f10f593
AminTabite <Aminepicode13@gmail.com>
2025-09-12 10:37:47 +0200

    elemento multimediale
    .

---
+++ src/Entities/ElementoMultimediale.java
@@ -0,0 +1,16 @@
+package Entities;
+
+public class ElementoMultimediale {
+    // attributi
+    private int durata;
+
+    //costruttori
+    public ElementoMultimediale() {
+        this.durata = durata;
+    }
+    //metodi
+
+    public void play() {
+
+    }
+}
---
+++ src/Entities/RegistrazioneAudio.java
@@ -0,0 +1,18 @@
+package Entities;
+
+public class RegistrazioneAudio extends ElementoMultimediale {
+    //attributi
+
+    private int volume;
+
+    // costruttore
+    public RegistrazioneAudio(int durata, int volume) {
+        super();
+        this.volume = volume;
+        
+
+    }
+
+    //metodi
+
+}
